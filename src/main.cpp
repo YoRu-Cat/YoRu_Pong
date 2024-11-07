c7ec31de1fa0be4c3d78e3effc7e01e723cc68b2
YoRu <tech.niko.coding@gmail.com>
2024-11-07 20:17:34 +0500

    Version 1.1

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,48 @@
 #include <raylib.h>
 
+class Ball
+{
+public:
+  float x = 0.0, y = 0.0;
+  int speedX = 0, speedY = 0;
+  int radius = 0;
+  void draw()
+  {
+    Color bgColor = {205, 25, 74, 255};
+    DrawCircle(x, y, radius, bgColor);
+  }
+  void move()
+  {
+    x += speedX;
+    y += speedY;
+    if (y + radius >= GetScreenHeight() || y - radius <= 0)
+    {
+      speedY *= -1;
+    }
+    if (x + radius >= GetScreenWidth() || x - radius <= 30)
+    {
+      speedX *= -1;
+    }
+  }
+};
+
+Ball ball;
+
 int main()
 {
-  int width = 1600;
-  int height = 900;
+  const int width = 1600;
+  const int height = 900;
   Color bgColor = {205, 25, 74, 255};
   SetConfigFlags(FLAG_WINDOW_UNDECORATED);
   InitWindow(width, height, "YoRu Pong");
   SetTargetFPS(120);
-  int ballX = width / 2;
-  int ballY = height / 2;
+  ball.radius = 15;
+  ball.x = width / 2;
+  ball.y = height / 2;
+  ball.speedX = 4;
+  ball.speedY = 4;
+  // int ballX = width / 2;
+  // int ballY = height / 2;
   bool drag = false;
   Vector2 dragOffset = {0, 0};
   // Game loop
@@ -19,42 +52,42 @@ int main()
     Vector2 mousePosition = GetMousePosition();
     // Vector2 is a Built in Element provided by Raylib that takes two float values, x and y and draw 2D calculations
     bool closeButtonClicked = false;
-    if (IsKeyDown(KEY_RIGHT))
-    {
-      ballX += 2;
-    }
-    else if (IsKeyDown(KEY_LEFT))
-    {
-      ballX -= 2;
-    }
-    else if (IsKeyDown(KEY_UP))
-    {
-      ballY -= 2;
-    }
-    else if (IsKeyDown(KEY_DOWN))
-    {
-      ballY += 2;
-    }
-    if (IsKeyDown(KEY_RIGHT) && IsKeyDown(KEY_UP))
-    {
-      ballX += 1;
-      ballY -= 1;
-    }
-    else if (IsKeyDown(KEY_RIGHT) && IsKeyDown(KEY_DOWN))
-    {
-      ballY += 1;
-      ballX += 1;
-    }
-    else if (IsKeyDown(KEY_LEFT) && IsKeyDown(KEY_UP))
-    {
-      ballY -= 1;
-      ballX -= 1;
-    }
-    else if (IsKeyDown(KEY_LEFT) && IsKeyDown(KEY_DOWN))
-    {
-      ballY += 1;
-      ballX -= 1;
-    }
+    // if (IsKeyDown(KEY_RIGHT))
+    // {
+    //   ballX += 2;
+    // }
+    // else if (IsKeyDown(KEY_LEFT))
+    // {
+    //   ballX -= 2;
+    // }
+    // else if (IsKeyDown(KEY_UP))
+    // {
+    //   ballY -= 2;
+    // }
+    // else if (IsKeyDown(KEY_DOWN))
+    // {
+    //   ballY += 2;
+    // }
+    // if (IsKeyDown(KEY_RIGHT) && IsKeyDown(KEY_UP))
+    // {
+    //   ballX += 1;
+    //   ballY -= 1;
+    // }
+    // else if (IsKeyDown(KEY_RIGHT) && IsKeyDown(KEY_DOWN))
+    // {
+    //   ballY += 1;
+    //   ballX += 1;
+    // }
+    // else if (IsKeyDown(KEY_LEFT) && IsKeyDown(KEY_UP))
+    // {
+    //   ballY -= 1;
+    //   ballX -= 1;
+    // }
+    // else if (IsKeyDown(KEY_LEFT) && IsKeyDown(KEY_DOWN))
+    // {
+    //   ballY += 1;
+    //   ballX -= 1;
+    // }
     // else if (IsKeyDown(KEY_SPACE))
     // {
     //   bgColor = {(unsigned char)GetRandomValue(0, 255), (unsigned char)GetRandomValue(0, 255), (unsigned char)GetRandomValue(0, 255), 255};
@@ -67,7 +100,7 @@ int main()
       {
         closeButtonClicked = true;
       }
-      else if (mousePosition.y >= 0 && mousePosition.y <= 10)
+      else if (mousePosition.y >= 0 && mousePosition.y <= 30)
       {
         drag = true;
         dragOffset = mousePosition;
@@ -89,22 +122,37 @@ int main()
     // increasing x value moves right
     // increasing y value moves down
 
+    // (0,0).______________(5,0)
+    //      |
+    //      |
+    //      |
+    //      |
+    // (0,5)|
+
     // Updating positions
-    // ballX += 2;
+    ball.move();
 
     // Drawing
     ClearBackground(RAYWHITE);
 
     BeginDrawing();
+
     ClearBackground(RAYWHITE);
-    DrawText("YoRu Pong", 10, 10, 20, DARKGRAY);
+    DrawText("YoRu Pong", 50, 10, 20, DARKGRAY);
 
     // Draw close button
     DrawRectangle(1570, 0, 30, 30, RED);
     DrawText("X", 1577, 2, 30, WHITE);
 
     // Draw the ball
-    DrawCircle(ballX, ballY, 15, bgColor);
+    // DrawCircle(ballX, ballY, 15, bgColor);
+    Rectangle rec = {0, 0, 1600, 900};
+    DrawRectangleLinesEx(rec, 5, bgColor);
+    Rectangle rec1 = {0, 0, 30, 900};
+    DrawRectangleLinesEx(rec1, 5, bgColor);
+    DrawRectangle(width - 35, height / 2 - 60, 25, 120, bgColor);
+    ball.draw();
+    DrawLine(width / 2, 0, width / 2, height, bgColor);
     EndDrawing();
 
     if (closeButtonClicked)
