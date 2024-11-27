baa1c1e9cbc97bb9b438049afe40881c3a6f44e7
YoRu <tech.niko.coding@gmail.com>
2024-11-27 20:46:12 +0500

    Version 2.2.6

--- src/YoRu_Pong.cpp
+++ src/YoRu_Pong.cpp
@@ -8,7 +8,7 @@ bool scoreSaved = false;
 bool gameStarted = false;
 bool showLeaderboard = false;
 bool showSettings = false; // Add this flag
-
+bool themePage = false;    // Add this flag
 // ball ka content
 float ballX, ballY;
 int ballSpeedX, ballSpeedY;
@@ -148,11 +148,24 @@ void DrawSavedScores()
       }
     }
   }
+  Color bgColor = {205, 25, 74, 255};
+  // Draw a background for the leaderboard
+  // DrawRectangle(GetScreenWidth() / 2 - 150, GetScreenHeight() / 2 - 150, 300, 250, Fade(bgColor, 0.3f));
 
-  DrawText("Top 5 Scores:", GetScreenWidth() / 2 - 100, GetScreenHeight() / 2 - 100, 20, DARKGRAY);
+  // Draw a border around the leaderboard
+  DrawRectangleLines(GetScreenWidth() / 2 - 400, GetScreenHeight() / 2 - 400, 800, 700, bgColor);
+
+  // Draw a title for the leaderboard
+  DrawText("Leaderboard", GetScreenWidth() / 2 - MeasureText("Leaderboard", 100) / 2, GetScreenHeight() / 2 - 360, 100, bgColor);
+
+  // Draw a decorative line under the title
+  DrawLine(GetScreenWidth() / 2 - 300, GetScreenHeight() / 2 - 260, GetScreenWidth() / 2 + 300, GetScreenHeight() / 2 - 260, bgColor);
+
+  // Draw the top 5 scores
+  DrawText("Top 5 Scores:", GetScreenWidth() / 2 - MeasureText("Top 5 Scores:", 80) / 2, GetScreenHeight() / 2 - 200, 80, bgColor);
   for (int i = 0; i < (scoreCount < 5 ? scoreCount : 5); i++)
   {
-    DrawText(TextFormat("%d", scores[i]), GetScreenWidth() / 2 - 100, GetScreenHeight() / 2 - 70 + i * 22, 20, DARKGRAY);
+    DrawText(TextFormat("%d", scores[i]), GetScreenWidth() / 2 - MeasureText(TextFormat("%d", scores[i]), 70) / 2, GetScreenHeight() / 2 - 100 + i * 80, 70, bgColor);
   }
 }
 
@@ -264,10 +277,72 @@ void DrawSettings()
   DrawText("Press BACKSPACE to go back", GetScreenWidth() / 2 - 100, GetScreenHeight() - 50, 20, DARKGRAY);
 
   EndDrawing();
-
+  if (themeButtonClicked)
+  {
+    themePage = true;
+    showSettings = false;
+  }
   if (IsKeyPressed(KEY_BACKSPACE))
   {
     showSettings = false;
+  }
+}
+
+void DrawThemePage()
+{
+  Color bgColor = {205, 25, 74, 255};
+  Vector2 mousePosition = GetMousePosition();
+  bool theme1Clicked = false;
+  bool theme2Clicked = false;
+  bool theme3Clicked = false;
+
+  Rectangle theme1Button = {(float)(GetScreenWidth() / 2 - 150), 150.0f, 300.0f, 50.0f};
+  Rectangle theme2Button = {(float)(GetScreenWidth() / 2 - 150), 220.0f, 300.0f, 50.0f};
+  Rectangle theme3Button = {(float)(GetScreenWidth() / 2 - 150), 290.0f, 300.0f, 50.0f};
+
+  if (CheckCollisionPointRec(mousePosition, theme1Button) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+  {
+    theme1Clicked = true;
+  }
+  if (CheckCollisionPointRec(mousePosition, theme2Button) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+  {
+    theme2Clicked = true;
+  }
+  if (CheckCollisionPointRec(mousePosition, theme3Button) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+  {
+    theme3Clicked = true;
+  }
+
+  BeginDrawing();
+  ClearBackground(RAYWHITE);
+
+  DrawRectangleRec(theme1Button, bgColor);
+  DrawText("Theme 1", theme1Button.x + theme1Button.width / 2 - MeasureText("Theme 1", 20) / 2, theme1Button.y + theme1Button.height / 2 - 10, 20, RAYWHITE);
+  DrawRectangleRec(theme2Button, bgColor);
+  DrawText("Theme 2", theme2Button.x + theme2Button.width / 2 - MeasureText("Theme 2", 20) / 2, theme2Button.y + theme2Button.height / 2 - 10, 20, RAYWHITE);
+  DrawRectangleRec(theme3Button, bgColor);
+  DrawText("Theme 3", theme3Button.x + theme3Button.width / 2 - MeasureText("Theme 3", 20) / 2, theme3Button.y + theme3Button.height / 2 - 10, 20, RAYWHITE);
+  DrawText("Press BACKSPACE to go back", GetScreenWidth() / 2 - 100, GetScreenHeight() - 50, 20, DARKGRAY);
+
+  EndDrawing();
+
+  if (theme1Clicked)
+  {
+    bgColor = {205, 25, 74, 255}; // Theme 1 color
+  }
+  if (theme2Clicked)
+  {
+    bgColor = {25, 205, 74, 255}; // Theme 2 color
+  }
+  if (theme3Clicked)
+  {
+    bgColor = {74, 25, 205, 255}; // Theme 3 color
+  }
+
+  if (IsKeyPressed(KEY_BACKSPACE))
+  {
+    themePage = false;
+    showSettings = true;
   }
 }
 
@@ -364,6 +439,10 @@ int main()
       else if (showSettings)
       {
         DrawSettings();
+      }
+      else if (themePage)
+      {
+        DrawThemePage();
       }
       else
       {
